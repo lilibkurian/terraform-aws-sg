bb826125dc1abd0a5fd0967ad9e7e88194075a34
lilib.kurian <lilib.kurian@gmail.com>
2020-05-06 09:52:00 -0400

    desc for ingress var

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "sg_name" {}
 variable "sg_desc" {}
 variable "sg_vpcid" {}
-variable "sg_ingress_ports" {}
+variable "sg_ingress_ports" {
+    description = "ingress ports for inbound traffic"
+}
 variable "sg_egress_ports" {
     description = "egress ports for outbound traffic"
 }
