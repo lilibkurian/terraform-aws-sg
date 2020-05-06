67f4a40e20caae4d2b0d4f157c57dbe8eb69fdfa
lilib.kurian <lilib.kurian@gmail.com>
2020-05-06 09:48:36 -0400

    desc for egress

--- variables.tf
+++ variables.tf
@@ -2,8 +2,9 @@ variable "sg_name" {}
 variable "sg_desc" {}
 variable "sg_vpcid" {}
 variable "sg_ingress_ports" {}
-variable "sg_egress_ports" {}
-
+variable "sg_egress_ports" {
+    description = "egress ports for outbound traffic"
+}
 variable sg_tags {
     description = "tags for security group"
 }
