1a8cc748e5c952d97b6bf0a6f92f0560a661945c
lilib.kurian <lilib.kurian@gmail.com>
2020-05-21 14:32:53 -0400

    output vars

--- output.tf
+++ output.tf
@@ -1,1 +1,13 @@
 //
+//
+output "security_group_description" {
+  value = aws_security_group.example.description
+}
+
+output "security_group_vpcid" {
+  value = aws_security_group.example.vpc_id
+}
+
+output "security_group_name" {
+  value = aws_security_group.example.name
+}
